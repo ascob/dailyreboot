74dd0e68bd28efc0b4a2029bda3e5899dd88b912
ascob <tobias.ilte@campus.tu-berlin.de>
2018-04-10 10:09:48 +0000

    cleanup

--- dailyreboot/src/dailyreboot.sh
+++ dailyreboot/src/dailyreboot.sh
@@ -7,23 +7,23 @@ reboottime=$(uci get dailyreboot.dailyreboot.time)
 offset=$(uci get dailyreboot.dailyreboot.offset)
 enabled=$(uci get dailyreboot.dailyreboot.enabled)
 
-
 #get hours of reboottime
 hours=$(($(date -d "$reboottime" +"%H")))
 
 #get minutes of reboottime
 minutes=$(($(date -d "$reboottime" +"%M")))
 
+#remove line containing 'dailyreboot' from crontab
 crontab -l | grep -v 'dailyreboot'  | crontab -
 
 if [ $enabled = "1" ]
 then
-    #set cronjob
+	#set cronjob
 	#write out current crontab
 	crontab -l > mycron
 	#echo new cron into cron file
 	echo "$minutes $hours * * * /usr/bin/dailyreboot $offset" >> mycron
 	#install new cron file
 	crontab mycron
-rm mycron
+	rm mycron
 fi
